978ce7686695e5abab5e5486f131659b347cf389
chatreejs <chanon29060@live.com>
2022-11-21 15:51:58 +0700

    feat: pass yes prompt in starship installer

--- setup/common/03-install-starship.sh
+++ setup/common/03-install-starship.sh
@@ -6,7 +6,7 @@ if [[ $OS == "Linux" ]]; then
     if ! command -v starship &> /dev/null
     then
         echo "🚀 Installing Starship"
-        curl -fsSL https://starship.rs/install.sh | sh
+        curl -fsSL https://starship.rs/install.sh | sh -s -- -y
         echo "✨ Starship installed"
     else
         echo "✅ Starship is installed"
